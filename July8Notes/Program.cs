83f3039b88c3293781294286217269cb3dd618e2
Jennifer <jebalas7@gmail.com>
2020-07-08 22:27:32 -0400

    Initialize repository

---
+++ July8Notes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace July8Notes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
